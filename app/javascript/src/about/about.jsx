be41029c0230c8f9c188fe7587e56ccb0e31047c
Collin Dapper <collindapper@gmail.com>
2022-09-26 00:30:13 -0700

    About Me Update

--- app/javascript/src/about/about.jsx
+++ app/javascript/src/about/about.jsx
@@ -21,7 +21,7 @@ class About extends React.Component {
 
             <Slide right>
             <div className="col-12 col-lg-8 my-md-auto pt-md-5 ps-md-5">
-              <p className="bodySection lh-lg text-black">Collin is an adaptive and creative junior full-stack web developer with proficiency in front-end programming languages such as: HTML, CSS, and ReactJS; Back-end programming knowledge in JavaScript and Ruby, as well as, experience conducting user research and collaborating with developers to build dynamic websites. Professional strengths include problem-solving, communication and time management. Analytical and leadership mindset from experience in Operations Management.</p>
+              <h4 className="bodySection lh-lg text-black">Collin is an adaptive and creative junior full-stack web developer with proficiency in front-end programming languages such as: HTML, CSS, and ReactJS; Back-end programming knowledge in JavaScript and Ruby, as well as, experience conducting user research and collaborating with developers to build dynamic websites. Professional strengths include problem-solving, communication and time management. Analytical and leadership mindset from experience in Operations Management.</h4>
             </div>
             </Slide>
 
